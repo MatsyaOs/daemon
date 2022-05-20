9ed4fba315c07bb6d15814eefa6e875da7045bba
Abhishek Sharma <sabhiron5@gmail.com>
2022-05-20 21:51:17 +0530

    ..

--- build/CMakeFiles/matsya-daemon_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/matsya-daemon_autogen"
-  "matsya-daemon_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/matsya-daemon_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
